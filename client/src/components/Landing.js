a839749dac468cf9c567506b67ada720a7507166
zohaib10 <zohaib.m.ahmad@gmail.com>
2018-06-11 12:07:28 -0700

    done

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -11,6 +11,8 @@ import { Accordion, AccordionItem } from 'react-sanfona';
 import { updateTasks, addNewCategory } from '../actions/index';
 import { bindActionCreators } from 'redux';
 import Popup from 'reactjs-popup';
+import abbriviations from '../data/abbriviations';
+import axios from 'axios';
 
 class Landing extends React.Component {
   constructor(props) {
@@ -21,7 +23,9 @@ class Landing extends React.Component {
       price: '',
       name: '',
       num: '',
-      newCategory: ''
+      newCategory: '',
+      rate: '',
+      demonym: ''
     };
   }
 
@@ -62,7 +66,7 @@ class Landing extends React.Component {
   }
 
   handleLocation() {
-    var add = 'Hawaii';
+    var add = 'Portugal';
     this.props.initialState.map(item => {
       if (item.id == this.props.location.pathname.split('/')[2]) {
         add = item.address;
@@ -77,6 +81,55 @@ class Landing extends React.Component {
   handlePriceChange = price => {
     this.setState({ price: price.target.value });
   };
+
+  componentDidMount() {
+    console.log('Attempting Currency Exchange');
+    var add = 'Portugal';
+    var countryCode = 'ABC';
+    var usCurrency = 'USD';
+    var denom = '';
+    var rate = '';
+    this.props.initialState.map(item => {
+      if (item.id == this.props.location.pathname.split('/')[2]) {
+        add = item.address;
+      }
+    });
+    add = add.split(',');
+    add = add[add.length - 1];
+
+    for (var i = 0; i < abbriviations.length; i++) {
+      if (abbriviations[i].name.common === add.trim()) {
+        countryCode = abbriviations[i].currency[0];
+        denom = abbriviations[i].demonym;
+      }
+    }
+    var endpoint = 'latest';
+    var access_key = '467589bf0bdc064fdb335d38ee436324';
+    axios
+      .get('http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key)
+      .then(response => {
+        var countryRate = parseFloat(response.data.rates[countryCode]);
+        var usRate = parseFloat(response.data.rates[usCurrency]);
+        rate = countryRate * parseFloat(1.0 / usRate);
+        console.log('here');
+        this.setState({ rate: rate, demonym: countryCode });
+      });
+  }
+
+  getCurrencyExchange() {
+    var name = this.handleLocation().split(',');
+    name = name[name.length - 1];
+    console.log(this.state.rate + ' hahah');
+    var rate = parseFloat(this.state.rate);
+    var rate = rate.toFixed(2);
+    return (
+      <ul className="collection">
+        <li className=" collection-item">
+          $1 USD - {rate} {name} {this.state.demonym}
+        </li>
+      </ul>
+    );
+  }
 
   mapCategoryInformation(item, num) {
     var tasks = this.props.initialState[num].categories;
@@ -112,7 +165,7 @@ class Landing extends React.Component {
 
   mapCategories() {
     var num = -1;
-
+    console.log(abbriviations);
     for (var i = 0; i < this.props.initialState.length; i++) {
       var item = this.props.initialState[i];
       if (item.id == this.props.location.pathname.split('/')[2]) {
@@ -256,11 +309,7 @@ class Landing extends React.Component {
                 <Typography>Currency Exchange Rate</Typography>
               </ExpansionPanelSummary>
               <ExpansionPanelDetails>
-                <Typography>
-                  Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
-                  Integer sit amet egestas eros, vitae egestas augue. Duis vel
-                  est augue.
-                </Typography>
+                <Typography>{this.getCurrencyExchange()}</Typography>
               </ExpansionPanelDetails>
             </ExpansionPanel>
           </Paper>
